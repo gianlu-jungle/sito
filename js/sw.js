7d94172635943c12f96b4d46f2620735d14d801b
gianlu-jungle <gianluigi.palmiotto@gmail.com>
2025-05-09 12:38:18 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,46 @@
-self.addEventListener('install', () => {
-  console.log('Service Worker installato');
+const CACHE_NAME = 'aijungle-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/scripts.js',
+  '/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Restituisci la risorsa dalla cache se disponibile
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
